432931055e81f128ea3e8f4c29c2054f88ae05c5
Joaquin Incio <joaquin.incio2002@outlook.com>
2023-05-13 22:07:43 -0500

    Cambios en css floating label

--- src/pages/login/components/login_field.js
+++ src/pages/login/components/login_field.js
@@ -6,19 +6,17 @@ function LogInField(){
     return(
         <Form>
             <Form.Group controlId = 'user-email_input'>
-                <FloatingLabel label = "Usuario o Correo" className='input'>
+                <FloatingLabel label = "Usuario o Correo">
                     <Form.Control type = "text" placeholder = 'Usuario o Correo'>
                     </Form.Control>
                 </FloatingLabel>
             </Form.Group>
-
+            <br></br>
             <Form.Group controlId = 'password_input'>
-                <Form.Label>
-                    Usuario o correo
-                </Form.Label>
-
-                <Form.Control type = "password">
-                </Form.Control>
+                <FloatingLabel label = "Contraseña">
+                    <Form.Control type = "password" placeholder = 'Contraseña'>
+                    </Form.Control>
+                </FloatingLabel>
             </Form.Group>
         </Form>
     );
